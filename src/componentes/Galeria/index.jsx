5e0bb001a055af29b9911f6cd4d1ec27f1331ab4
aoc <michaelmoranesul@gmail.com>
2023-04-22 20:04:58 -0300

    criando componente cards

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Tags from '../Tags'
 import styles from "./Galeria.module.scss"
 import fotos from "./fotos.json"
-import favorito from "./favorito.png"
-import open from "./open.png"
+import Cards from './Cards'
 
 export default function Galeria() {
   console.log("nova branch criada para alteracoes no projeto")
@@ -11,29 +10,7 @@ export default function Galeria() {
         <section className={styles.galeria}>
             <h2>navegue pela galeria</h2>
             <Tags />
-            <ul className={styles.galeria__cards}>
-                {fotos.map((foto) => {
-                    return (
-                        <li key={foto.id} className={styles.galeria__card}>
-                            <img
-                            className={styles.galeria__imagem} 
-                            src={foto.imagem} 
-                            alt={foto.titulo} 
-                            />
-                            <p className={styles.galeria__descricao}>
-                                {foto.titulo}
-                            </p>
-                            <div>
-                                <p>{fotos.creditos}</p>
-                                <span>
-                                    <img src={favorito} alt="icone coracao de curtir" />
-                                    <img src={open} alt="icone de abrir modal" />
-                                </span>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
+            <Cards itens={fotos} styles={styles} />
         </section>
   )
 }
