95d0c622266bdaef5624d01e076996b85c28e806
aoc <michaelmoranesul@gmail.com>
2023-04-22 18:56:17 -0300

    primeira mudanca na branch vertente, ponto de criacao da galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Tags from '../Tags'
 
 export default function Galeria() {
+  console.log("nova branch criada para alteracoes no projeto")
   return (
         <section>
             <h2>navegue pela galeria</h2>
