38ca007ed9e868f81bd56d1ae87f35ab0e7a88b7
aoc <michaelmoranesul@gmail.com>
2023-04-22 15:10:34 -0300

    criando componentes galeria rodape e tags

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,6 +1,8 @@
 import banner from "./banner.png"
 import Cabecalho from "../../componentes/Cabecalho"
 import Menu from "../../Menu"
+import Galeria from "../../componentes/Galeria"
+import Rodape from "../../componentes/Rodape"
 import styles from "./PaginaInicial.module.scss"
 
 export default function PaginaInicial() {
@@ -16,6 +18,10 @@ export default function PaginaInicial() {
                 </div>
              </section>
           </main>
+          <div>
+            <Galeria />
+          </div>
+          <Rodape />
         </>
         )
 }
