d1b64305146b38f9fdbe294e51c4c4360fd9045a
aoc <michaelmoranesul@gmail.com>
2023-04-24 23:16:33 -0300

    uso do metodo filter e map

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -3,14 +3,23 @@ import Tags from '../Tags'
 import styles from "./Galeria.module.scss"
 import fotos from "./fotos.json"
 import Cards from './Cards'
+import { useState } from 'react'
 
 export default function Galeria() {
-  console.log("nova branch criada para alteracoes no projeto")
+ const [itens, setItens] = useState(fotos);
+ const tags = [...new Set(fotos.map((valor) => valor.tag))];
+
+ const filtraFotos = (tag) => {
+   const novasFotos = fotos.filter((foto) => {
+    return foto.tag === tag;
+   })
+   setItens(novasFotos);
+ }
   return (
         <section className={styles.galeria}>
             <h2>navegue pela galeria</h2>
-            <Tags />
-            <Cards itens={fotos} styles={styles} />
+            <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens}/>
+            <Cards itens={itens} styles={styles} />
         </section>
   )
 }
